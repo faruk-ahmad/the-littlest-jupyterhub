265e80c052e50f612e283028c65ef9d16b886a1b
David Lowry-Duda <david.j.lowry@gmail.com>
2019-02-14 10:01:03 +0000

    none --> None

--- integration-tests/test_hub.py
+++ integration-tests/test_hub.py
@@ -6,7 +6,7 @@ try:
     from secrets import token_hex
 except ImportError:
     from os import urandom
-    def token_hex(nbytes=none):
+    def token_hex(nbytes=None):
         return urandom(nbytes).hex()
 
 import pytest
--- tljh/installer.py
+++ tljh/installer.py
@@ -9,7 +9,7 @@ try:
     from secrets import token_hex
 except ImportError:
     from os import urandom
-    def token_hex(nbytes=none):
+    def token_hex(nbytes=None):
         return urandom(nbytes).hex()
 
 import subprocess
